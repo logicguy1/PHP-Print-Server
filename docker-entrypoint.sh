c905961969f84825cd76863f39285974a564e07f
Drillenissen <logicguy.mailandcontact@gmail.com>
2026-05-20 16:38:12 +0200

    Added upload folder to seed list

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+mkdir -p /var/www/data /var/www/html/public/uploads
 chown -R www-data:www-data /var/www/data /var/www/html/public/uploads
 chmod 755 /var/www/data /var/www/html/public/uploads
 
